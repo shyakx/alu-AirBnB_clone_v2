bbc6e27565f0ba1c787a7e47311a82ec314c932e
Steven SHYAKA <s.shyaka1@alustudent.com>
2024-02-19 20:29:28 +0000

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,47 +1,42 @@
 #!/usr/bin/python3
-"""Start web application with two routings
 """
-
-from flask import Flask
+Script that starts a Flask web application:
+listening on 0.0.0.0, port 5000
+with Fifth routes
+"""
+from flask import Flask, escape, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    """Return string when route queried
-    """
-    return 'Hello HBNB!'
+    """ First Route that display Hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
-def hbnb():
-    """Return string when route queried
-    """
-    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Second Route that display HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def hello_text(text):
+    """ Third Route that display C and text"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text):
+    """ Fourth Route that display C and text """
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>')
-def number(n=None):
-    """Allow request if path variable is a valid integer
-    """
-    return str(n) + ' is a number'
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_number(n):
+    """ Fifth Route that display C and text """
+    return "%d is a number" % n
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
-
